6345f47ad1e09ed679d3f6fd77725549ae82b2dc
Alyona275 <alena.kim.220795@gmail.com>
2021-03-08 21:02:21 +0900

    2.4.2 commit

--- src/main/java/CRUDapplication/service/UserDetailsServiceImpl.java
+++ src/main/java/CRUDapplication/service/UserDetailsServiceImpl.java
@@ -1,12 +1,18 @@
 package CRUDapplication.service;
 
 import CRUDapplication.dao.UserDao;
+import CRUDapplication.model.Role;
 import CRUDapplication.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -19,26 +25,15 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        User user = userDao.findByUsername(s);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
 
-        if (user == null) {
-            throw new UsernameNotFoundException(String.format("User '%s' not found", s));
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        for (Role role : user.getRoles()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
         }
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                user.getAuthorities());
-    }
 
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userDao.findByUsername(username);
-//
-//        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-//
-//        for (Role role : user.getRoles()) {
-//            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
-//        }
-//
-//        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
-//    }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
 }
